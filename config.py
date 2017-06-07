b26057ea8883a33b8eed1dfa8ca677439e3c9c7e
Lars Rustand <rustand.lars@gmail.com>
2017-06-07 21:39:01 +0200

    add config file and error messages

---
+++ config.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+repo_path   = "/srv/ftp/repos/aurbuild"
+db_path     = repo_path+"/"+repo_path.split("/")[-1]+".db.tar.gz"
+build_path  = "/tmp/aur-repo-builder/build"
+pkg_list    = "/etc/aur-repo-builder/package-list"
